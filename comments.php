78edb051bbc27b16532fd47d82248acd6d7a5d67
MohammedAnasuddinZaid <mohammedanasuddinzaid256@gmail.com>
2026-06-21 18:28:07 +0530

    till lecture 20

---
+++ comments.php
@@ -0,0 +1,20 @@
+<?php 
+
+// $name="anas";                 
+// $city = "noida";     
+// $age = 30;                
+// echo $name;               
+         
+
+
+?>
+
+
+<?php 
+/*
+$Name="je";                 
+$City = "noida";     
+$Age = 30;                
+echo $Name;              
+*/
+?>
---
+++ constant.php
@@ -0,0 +1,12 @@
+<?php 
+
+// const beta = "Abc";
+
+
+define("data","anas");
+define("data","dam")
+
+
+echo data
+
+?>
