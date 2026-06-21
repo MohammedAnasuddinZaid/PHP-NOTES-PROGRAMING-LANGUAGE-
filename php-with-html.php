f738d484b5fb952c52fdc691690c1bb94d69b129
MohammedAnasuddinZaid <mohammedanasuddinzaid256@gmail.com>
2026-06-21 15:35:43 +0530

    till lecture 15

---
+++ php-with-html.php
@@ -0,0 +1,21 @@
+<?php 
+
+$color_3="skyblue";
+
+echo "<h1>php with html</h1>";
+
+?>
+
+<?php 
+
+$name="anas";
+
+echo "<h1 style='color:red' my name is >$name</h1>";
+
+?>
+
+<h1 style="color:<?php echo $color_3 ?>">
+
+My real name is <?php echo $name; ?>
+
+</h1>
