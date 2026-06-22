aebd9c22c1dbd8edd65e832569d0353dbf7394cf
MohammedAnasuddinZaid <mohammedanasuddinzaid256@gmail.com>
2026-06-23 03:59:02 +0530

    37

---
+++ function.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+function dummy(){
+    
+    echo "<h1>USER DETAILS</h1>";
+}
+
+
+function Displayname(){
+    dummy();
+    echo "THIS IS ANAS <br>";
+    echo "THE GMAIL IS <HR>";
+    
+
+}
+
+Displayname();
+Displayname();
+
+Displayname();
+Displayname();
+
+Displayname();
+Displayname();
+
+Displayname();
+Displayname();
+
+
+
+?>
