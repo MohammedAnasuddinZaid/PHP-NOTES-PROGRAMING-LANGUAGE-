9f9d7dc4efbb60cb7d081531f4805508647ed2df
MohammedAnasuddinZaid <mohammedanasuddinzaid256@gmail.com>
2026-06-23 05:44:35 +0530

    43 lectures done

---
+++ nested-function.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+function firsttest(){
+    echo " first test function called </br>";
+    
+    function secondfunction(){
+        echo "second function called";
+    }
+    secondfunction();
+
+}
+
+firsttest();
+?>
+
+
---
+++ variable-functions.php
@@ -0,0 +1,21 @@
+<?php 
+
+function test(){
+
+echo "anas";
+
+}
+
+$nar="test";
+
+// $nar();
+
+function main($fu){
+    $fu();
+}
+
+main($nar)
+
+
+
+?>
