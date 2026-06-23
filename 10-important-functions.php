3362de215b634503646b0168f8e0d4dbcc5c5ef8
MohammedAnasuddinZaid <mohammedanasuddinzaid256@gmail.com>
2026-06-23 07:52:06 +0530

    44

---
+++ 10-important-functions.php
@@ -0,0 +1,29 @@
+<?php 
+
+// $humm="anas bhai";
+// echo strlen($humm);
+
+// $bar=10;
+// var_dump($bar);
+
+// echo date("d,l,m,M"); //check on google ssaying date function php
+
+// echo phpinfo();
+
+// $var="abc";
+// echo is_string($var);
+
+// $var=24;
+// echo is_int($var);
+
+// echo rand();
+
+// echo "before";
+// // die();
+// // exit();
+// echo "After";
+
+// $str="code step by step ";
+// echo substr($str,5);
+
+?>
