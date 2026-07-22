24c0421f4e5e24bc80179ec1607e91712cae43ae
MohammedAnasuddinZaid <mohammedanasuddinzaid256@gmail.com>
2026-07-22 14:21:13 +0530

    made a whole website with PHP

---
+++ Website/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="contianer">
+        <?php 
+        include_once("header.php")
+        ?>
+        <h1>HOME PAGE</h1>
+        <?php 
+        include_once("footer.php")
+    ?>
+    
+</body>
+</html>
